5822d5adb948bded231a63ba6ad09b6a96e90035
Biswajit Roy <broy16431@gmail.com>
2023-02-17 13:37:00 +0530

    code_fix_biswa

--- prime.js
+++ prime.js
@@ -1,4 +1,4 @@
-let num=25;
+let num=4;
 let prime=((num)=>{
     if(num<=1)
     {
@@ -9,7 +9,7 @@ let prime=((num)=>{
         let factors=0;
         for(let a=2;a<num;a++)
         {
-            if(num%2===0)
+            if(num%a===0)
             {
                 factors++;
             }
